b3678564a45fa037c2c723f343025db16372974e
Ethan Brooks <ethanabrooks@gmail.com>
2020-07-23 08:42:19 -0400

    add name to lookup

--- src/main/scala/runs/manager/LookupCommand.scala
+++ src/main/scala/runs/manager/LookupCommand.scala
@@ -10,18 +10,22 @@ import runs.manager.Main._
 
 import scala.language.postfixOps
 
+case class LookupPair(name: String, result: String)
+
 trait LookupCommand {
   def lookupCommand(pattern: Option[String], active: Boolean, field: String)(
     implicit blocker: Blocker,
     xa: H2Transactor[IO]
-  ): IO[ExitCode] = {
+  ): IO[ExitCode] =
     for {
       conditions <- selectConditions(pattern, active)
-      results <- (fr"SELECT" ++ Fragment.const(field) ++ fr"FROM runs" ++ conditions)
-        .query[String]
+      results <- (fr"SELECT name," ++ Fragment.const(field) ++ fr"FROM runs" ++ conditions)
+        .query[LookupPair]
         .to[List]
         .transact(xa)
-      _ <- results.traverse(putStrLn)
+      _ <- results.traverse {
+        case LookupPair(name, result) =>
+          putStrLn(s"${Console.BOLD}$name:${Console.RESET}\t$result")
+      }
     } yield ExitCode.Success
-  }
 }
