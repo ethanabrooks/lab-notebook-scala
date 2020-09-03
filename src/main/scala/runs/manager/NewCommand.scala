526951a50c4d50588a70100cb31721081e6e6674
Ethan Brooks <ethanabrooks@gmail.com>
2020-09-03 13:29:46 -0400

    take out repeat \s in config

--- src/main/scala/runs/manager/NewCommand.scala
+++ src/main/scala/runs/manager/NewCommand.scala
@@ -258,7 +258,7 @@ trait NewCommand {
       "--volume",
       s"$hostVolume:$containerVolume",
       image
-    ) ++ config.fold(List[String]())(_.split(" ").toList)
+    ) ++ config.fold(List[String]())(_.replaceAll("""\s+""", " ").split(" ").toList)
     Process[IO](dockerRun.head, dockerRun.tail) ># captureOutput
   }
 
