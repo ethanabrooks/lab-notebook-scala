a49e0520f84cc2cdcbf56af91827066af752e6d5
Ethan Brooks <ethanabrooks@gmail.com>
2021-02-26 20:56:18 -0500

    got things to compile with rudimentary argument parser

--- src/main/scala/runs/manager/Main.scala
+++ src/main/scala/runs/manager/Main.scala
@@ -43,154 +43,9 @@ object Main
     with KillCommand
     with ReproduceCommand {
 
-  val captureOutput: Pipe[IO, Byte, String] = fs2.text.utf8Decode[IO]
-  implicit val cs: ContextShift[IO] =
-    IO.contextShift(ExecutionContexts.synchronous)
-  implicit val runner: ProcessRunner[IO] = new JVMProcessRunner
-
-  def pause(implicit yes: Boolean): IO[Unit] = if (yes) IO.unit else readLn.void
-
-  def putStrLnBold(x: String): IO[Unit] =
-    putStrLn(Console.BOLD + x + Console.RESET)
-
-  def putStrLnRed(x: String): IO[Unit] =
-    putStrLn(Console.RED + x + Console.RESET)
-
-  def putStrLnGreen(x: String): IO[Unit] =
-    putStrLn(Console.GREEN + x + Console.RESET)
-
-  def check(requireYes: Boolean = false)(implicit yes: Boolean): IO[Boolean] = {
-    val noPattern: Regex = "[nN]o?".r
-    val yesPattern: Regex = "[yY](:?es)?".r
-    if (yes) IO.pure(true) else
-    for {
-      response <- readLn
-    } yield
-      if (requireYes) yesPattern.matches(response)
-      else !noPattern.matches(response)
-  }
-
-  def selectConditions(pattern: Option[String], active: Boolean)(
-    implicit blocker: Blocker
-  ): IO[Fragment] = {
-    val nameLikePattern: Option[Fragment] =
-      pattern.map(p => fr"name LIKE $p")
-    if (active) {
-      activeContainers(None).map {
-        case Nil => fr"WHERE FALSE"
-        case activeIds =>
-          val fragments: List[Fragment] = activeIds
-            .map(id => {
-              val condition = fr"containerId LIKE ${id + "%"}"
-              nameLikePattern.fold(condition)(condition ++ fr"AND" ++ _)
-            })
-          val orClauses = Fragments.or(fragments.toArray.toIndexedSeq: _*)
-          fr"WHERE" ++ orClauses
-      }
-    } else {
-      IO.pure(nameLikePattern.fold(fr"")(fr"WHERE" ++ _))
-    }
-  }
-
-  def nameContainerQuery(
-    conditions: Fragment
-  ): ConnectionIO[List[NameContainer]] =
-    (fr"SELECT name, containerId FROM runs" ++ conditions)
-      .query[NameContainer]
-      .to[List]
-
-  def killContainers(containers: List[String])(
-    implicit blocker: Blocker,
-    yes: Boolean
-  ): IO[Option[ProcessResult[Any, Any]]] = containers match {
-    case Nil        => IO.pure(None)
-    case containers => killProc(containers).checkThenPerform()
-  }
-
-  def rmStatement(names: List[String]): ConnectionIO[_] = {
-    val conditions = names.map(name => fr"name = $name")
-    val statement = fr"DELETE FROM runs where" ++ Fragments
-      .or(conditions.toIndexedSeq: _*)
-    statement.update.run
-  }
-
-  def procToList(
-    proc: ProcessImplO[IO, String]
-  )(implicit blocker: Blocker): IO[List[String]] =
-    proc
-      .run(blocker)
-      .map(_.output)
-      .map {
-        case "" => List()
-        case nonEmpty =>
-          nonEmpty.split("\n").map(_.stripLineEnd).toList
-      }
-
-  def dockerPsProc(label: Option[String]): ProcessImplO[IO, String] =
-    Process[IO](
-      "docker",
-      List("ps", "-q") ++ label
-        .fold(List[String]())(l => List("--filter", s"label=$l"))
-    ) ># captureOutput
-
-  def activeContainers(
-    label: Option[String]
-  )(implicit blocker: Blocker): IO[List[String]] =
-    procToList(dockerPsProc(label))
-
-  def dockerVolumeLsProc(name: String): ProcessImplO[IO, String] = {
-    Process[IO](
-      "docker",
-      List("volume", "ls", "-q", "--filter", "name=^%s$".format(name))
-    ) ># captureOutput
-  }
-
-  def existingVolumes(
-    names: List[String]
-  )(implicit blocker: Blocker): IO[List[String]] =
-    names
-      .traverse(n => procToList(dockerVolumeLsProc(n)))
-      .map(_.flatten)
-
-  def rmVolumeProc(volumes: List[String]): Process[IO, _, _] =
-    Process[IO]("docker", List("volume", "rm") ++ volumes)
-
-  def killProc(ids: List[String]): Process.ProcessImpl[IO] =
-    Process[IO]("docker", "kill" :: ids)
-
-  def followProc(id: String): Process[IO, _, _] =
-    Process[IO]("docker", List("logs", "--follow", id))
-
-  def runInsert(newRows: List[RunRow])(implicit blocker: Blocker,
-                                       xa: H2Transactor[IO]): IO[Unit] = {
-    RunRow.mergeCommand.updateMany(newRows).transact(xa).void
-  }
-
   override def main: Opts[IO[ExitCode]] = opts.map {
     case AllOpts(dbPath, server, y, sub) =>
-      implicit val yes: Boolean = y
-      val uri: String =
-        "jdbc:h2:%s%s;DB_CLOSE_DELAY=-1".format(if (server) {
-          s"tcp://localhost/"
-        } else {
-          ""
-        }, dbPath)
-
-      Blocker[IO].use { b =>
-        implicit val blocker: Blocker = b
-        val transactor: Resource[IO, H2Transactor[IO]] = for {
-          ce <- ExecutionContexts.fixedThreadPool[IO](32) // our connect EC
-          xa <- H2Transactor.newH2Transactor[IO](
-            uri, // connect URL
-            "sa", // username
-            "", // password
-            ce, // await connection here
-            blocker // execute JDBC operations here
-          )
-        } yield xa
-        transactor.use { x =>
-          implicit val xa: H2Transactor[IO] = x
-          sub match {
+          su match {
             case NewOpts(
                 containerVolume: String,
                 description: Option[String],
@@ -229,7 +84,7 @@ object Main
                 pattern: String,
                 active: Boolean,
                 description: Option[String],
-                resample: Boolean,
+                resample: Boolea,
                 dockerRunCommand: List[String],
                 containerVolume: String,
                 interpreter: String,
@@ -237,8 +92,6 @@ object Main
                 follow: Boolean
                 ) =>
               IO.raiseError(new RuntimeException("not implemented"))
-          }
         }
       }
-  }
 }
